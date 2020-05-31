58ec0393ea6b390c2b6eee84f9ac2889a0668282
vegadevh <00148816@uca.edu.sv>
2020-05-31 17:17:04 -0600

    classes.h mod

--- Code/classes.h
+++ Code/classes.h
@@ -1,6 +1,6 @@
 enum lines {NOLINE,SINGLELINE,DOUBLELINE};
 enum modes {NOMODE,INT_FLOAT,INT_FLOAT_FLOAT,INT_INT_INT_INT};
-enum parameters {ADJECTIVE_VELOCITY,DYNAMIC_VISCOSITY,DENSITY,EXTERNAL_FORCE_X,EXTERNAL_FORCE_Y};
+enum parameters {TAO_CONST,K_CONST,LAMBDA_CONST,V_CONST,PSI_CONST, DELTA_CONST,ETA_CONST};
 enum sizes {NODES,ELEMENTS,DIRICHLET};
 enum coords {EQUIS,YE};
 
@@ -109,7 +109,7 @@ class condition: public item{
 };
 
 class mesh{
-        float parameters[5];
+        float parameters[7];
         int sizes[3];
         node *node_list;
         element *element_list;
@@ -117,12 +117,15 @@ class mesh{
         condition *dirichlet_list;
         //condition *neumann_list;
     public:
-        void setParameters(float u_bar,float nu, float rho, float f_x, float f_y){
-            parameters[ADJECTIVE_VELOCITY]=u_bar;
-            parameters[DYNAMIC_VISCOSITY]=nu;
-            parameters[DENSITY]=rho;
-            parameters[EXTERNAL_FORCE_X]=f_x;
-            parameters[EXTERNAL_FORCE_Y]=f_y;
+    //TAO_CONST,K_CONST,LAMBDA_CONST,V_CONST,PSI_CONST, DELTA_CONST,ETA_CONST
+        void setParameters(float tao,float k, float lambda, float v, float psi, float delta, float eta){
+            parameters[TAO_CONST]=tao;
+            parameters[K_CONST]=k;
+            parameters[LAMBDA_CONST]=lambda;
+            parameters[V_CONST]=v;
+            parameters[PSI_CONST]=psi;
+            parameters[DELTA_CONST]=delta;
+            parameters[ETA_CONST]=eta;
         }
         void setSizes(int nnodes,int neltos,int ndirich){
             sizes[NODES] = nnodes;
